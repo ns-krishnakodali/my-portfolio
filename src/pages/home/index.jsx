f9a47792638045272e78439bad6e95700b24f894
Krishna Kodali <krishnakodali81@gmail.com>
2025-05-09 05:10:51 -0400

    Update: Home page styling and route requests to react router

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -27,29 +27,31 @@ export const Home = () => {
   }, []);
 
   return (
-    <div
-      id="home"
-      className="home-container"
-      style={{ minHeight: `calc(100vh - ${navbarHeight}px)` }}
-    >
-      <div className="home__image">
-        <img
-          src="assets/generic/display-image.jpeg"
-          alt="Display Image"
-          className="display-image"
-          loading="eager"
-        />
-      </div>
-      <div className="home__details">
-        <h1 className="home__name">Hi, I'm Krishna</h1>
-        <h3 className="home__title">
-          <span>Software Engineer</span>
-          <span className="home__divider">|</span>
-          <span>AI/ML Enthusiast</span>
-        </h3>
-        <p className="about-me">{HOME_ABOUT_ME}</p>
-        <SectionLink path="/about" text="Know More" showArrow />
-        <LogoDetails />
+    <div className="home">
+      <div
+        id="home"
+        className="home-container"
+        style={{ minHeight: `calc(100vh - ${navbarHeight}px)` }}
+      >
+        <div className="home__image">
+          <img
+            src="assets/generic/display-image.jpeg"
+            alt="Display Image"
+            className="display-image"
+            loading="eager"
+          />
+        </div>
+        <div className="home__details">
+          <h1 className="home__name">Hi, I'm Krishna</h1>
+          <h3 className="home__title">
+            <span>Software Engineer</span>
+            <span className="home__divider">|</span>
+            <span>AI/ML Enthusiast</span>
+          </h3>
+          <p className="about-me">{HOME_ABOUT_ME}</p>
+          <SectionLink path="/about" text="Know More" showArrow />
+          <LogoDetails />
+        </div>
       </div>
     </div>
   );
